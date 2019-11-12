f88d46cc087af2038522a34168a9f97a349ce4d1
Tigerots <Tigerots@users.noreply.github.com>
2019-11-12 11:26:30 +0800

    测试修改

--- TcpForm.py
+++ TcpForm.py
@@ -47,7 +47,7 @@ class Form1Win(tcplogic.TcpLogic, udplogic.UdpLogic):
         # 增加公司服务器默认IP
         self.comboBox_TCP.addItems(self.TcpTypeList)
 
-        self.textEdit_Send.setText("Hello the world...")
+        self.textEdit_Send.setText("Hello my world...")
 
 
         # 执行初始化函数
